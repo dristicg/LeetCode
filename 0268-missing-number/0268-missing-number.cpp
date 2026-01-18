9f034aeb84dd2ff340dd8fde5c8b2b520d967a65
Dristi Gupta <dristi.gupta.cg@gmail.com>
2026-01-18 07:49:25 +0530

    Time: 0 ms (100%), Space: 21.8 MB (37.26%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int actualSum = n * (n + 1) / 2;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+
+        int missNo = actualSum - sum;
+        return missNo;
+    }
+};
