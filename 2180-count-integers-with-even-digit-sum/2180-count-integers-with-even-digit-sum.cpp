2c7d91087813c2499bf3b437564f5d672b8ac410
Dristi Gupta <dristi.gupta.cg@gmail.com>
2026-03-21 22:49:33 +0530

    Time: 0 ms (100%), Space: 7.9 MB (9.8%) - LeetHub

---
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countEven(int num) {
+        int positiveInteger = 0;
+        for (int i = 1; i <= num; i++) {
+            int temp = i;
+            int sum = 0;
+
+            while (temp > 0) {
+                sum = sum + temp % 10;
+                temp = temp / 10;
+            }
+            if (sum % 2 == 0) {
+                positiveInteger++;
+            }
+        }
+        return positiveInteger;
+    }
+};
