9b10141d0dc8f8709a909809aa6143b70a957ced
Dristi Gupta <dristi.gupta.cg@gmail.com>
2026-05-03 18:45:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,14 +1,31 @@
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         if (s.length() != goal.length())
+//             return false;
+
+//         string temp = s + s;
+
+//         if (temp.find(goal) != string::npos)
+//             return true;
+
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool rotateString(string s, string goal) {
         if (s.length() != goal.length())
             return false;
 
-        string temp = s + s;
+        for (int i = 0; i < s.length(); i++) {
+            if( s==goal) return true;
 
-        if (temp.find(goal) != string::npos)
-            return true;
-
+            char first = s[0];
+            s.erase(s.begin());
+            s.push_back(first);
+        }
         return false;
     }
 };
