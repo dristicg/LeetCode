a479373106f297822669e94464e92df65b17c748
Dristi Gupta <dristi.gupta.cg@gmail.com>
2026-05-13 22:45:42 +0530

    Time: 5 ms (78.76%), Space: 93.3 MB (83.19%) - LeetHub

--- 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums, int limit) {
+
         int n = nums.size();
+
         vector<int> diff(2 * limit + 2, 0);
 
-        for (int i = 0; i < n / 2; ++i) {
-            int a = nums[i], b = nums[n - 1 - i];
-            int low = min(a, b) + 1;
-            int high = max(a, b) + limit;
-            int sum = a + b;
+        for(int i = 0; i < n / 2; i++) {
 
-            diff[2] += 2;           // assume 2 moves for all
-            diff[low] -= 1;         // 1 move from low to sum
-            diff[sum] -= 1;         // 0 move at sum
-            diff[sum + 1] += 1;     // back to 1 move after sum
-            diff[high + 1] += 1;    // back to 2 moves after high
+            int a = nums[i];
+            int b = nums[n - 1 - i];
+
+            int low = min(a, b);
+            int high = max(a, b);
+
+            // Initially assume 2 moves
+            diff[2] += 2;
+
+            // 1 move range starts
+            diff[low + 1] -= 1;
+
+            // 0 move at exact sum
+            diff[a + b] -= 1;
+
+            // back to 1 move
+            diff[a + b + 1] += 1;
+
+            // back to 2 moves
+            diff[high + limit + 1] += 1;
         }
 
-        int res = INT_MAX, curr = 0;
-        for (int i = 2; i <= 2 * limit; ++i) {
-            curr += diff[i];
-            res = min(res, curr);
+        int ans = INT_MAX;
+        int moves = 0;
+
+        for(int s = 2; s <= 2 * limit; s++) {
+
+            moves += diff[s];
+
+            ans = min(ans, moves);
         }
-        return res;
+
+        return ans;
     }
 };
